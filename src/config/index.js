e4a14942881f2159988fdacbaa7253409a41352e
xcho7i <xchozoiy@gmail.com>
2025-09-10 17:43:53 +0900

    fix:name to yev

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-    developerName: "Andrii Hryholiait",
-    developerDescription: "Andrii Hryholiait is a Extension Developer and Full Stack Developer",
+    developerName: "Yevhenii Ivashchenko",
+    developerDescription: "Yevhenii Ivashchenko is a Extension Developer and Full Stack Developer",
     developerKeywords: ["Extension Developer", "Full-Stack Developer", "Scraping Expert"],
     developerImage: "/hero.jpg",
     developerEmail: "xchozoiy@gmail.com",
